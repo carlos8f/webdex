ea77a25cd55539aabb04479781937ff9135c2f41
Carlos Rodriguez <carlos@s8f.org>
2015-06-28 19:45:13 -0700

    new developments

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all: clean
 	./node_modules/.bin/electron-packager app Webdex --platform=darwin --arch=x64 --version=0.28.3 --out=build
+	rm -Rf /Applications/Webdex.app
+	cp -Rf ./build/Webdex.app /Applications
+	open /Applications/Webdex.app
 
 clean:
 	rm -Rf build/*
--- app/main.js
+++ app/main.js
@@ -10,22 +10,20 @@ var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
+  app.quit();
 });
 
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1280, height: 720});
 
   // and load the index.html of the app.
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
   // Open the devtools.
-  mainWindow.openDevTools();
+  // mainWindow.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
